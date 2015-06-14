51e7d67aefeb3e1838f6a5f58c03dbfe346a8cf1
cindysantika <cindysantika04@gmail.com>
2015-06-15 05:13:02 +0700

    tgl

--- home.php
+++ home.php
@@ -2,9 +2,14 @@ coba
 <?php include "cek_session.php"; ?>
 <div class="post">
 	<h2 class="title"><a href="#">Selamat Datang </a></h2>
-	<p class="meta"><em>Sunday, April 26, 2009 7:27 AM Posted by <a href="#">Someone</a></em></p>
+	<p class="meta"><em>Monday, Juni 15, 2015 4.57 AM Posted by <a href="#">Someone</a></em></p>
 	<div class="entry">
 		<p>Selamat datang di Jurnal Matakuliah Teknik Informatika Universitas Negeri Surabaya.. </p>
-	</div>
+	<p>Jurusan Teknik Informatika adalah satu dari lima jurusan yang ada di selingkung  Fakultas Teknik (FT) Universitas Negeri Surabaya (Unesa). 
+Jurusan Teknik Elektro saat ini menyelenggarakan lima (5) Program Studi berjalan yaitu Program Studi
+S1 Pendidikan Teknik Informatika, dan D3 Manajemen Informatika. Saat ini juga telah turun pengajuan dua (2) program studi berbasis IT, 
+Prodi S1 Teknik Informatika dan S1 Sistem Informasi.
+</p>
+</div>
 </div>
 
