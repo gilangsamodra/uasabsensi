cbe53553911fbf557250ef32cfb4aab9f249cca1
MaratulFarikhah <arikfarika@yahoo.com>
2015-06-17 14:26:58 -0700

    embo

--- input_jurnal.php
+++ input_jurnal.php
@@ -15,11 +15,69 @@
 			<td width="24%" align="left" colspan="6">Tanggal : <input type="text" name="tanggal"  value="<?php if(empty($_POST['tgl'])){ echo $tanggal;}else{ echo "$_POST[tgl]$_GET[tgl]"; }?>" size="11"><a href="javascript:void(0)" onClick="if(self.gfPop)gfPop.fPopCalendar(document.postform.tanggal);return false;" ><img name="popcal" align="absmiddle" src="calender/calbtn.gif" width="34" height="29" border="0" alt=""></a></td>
 		</tr>
 		<tr>
+			<td>Materi</td>
+			<td><input type="text" size="20" name="nm_mk" /></td>
+		</tr>
+		<tr>
+		<td>Kelas</td>
+			<td>
+			<select name="kelas">
+			<option value="0" selected="selected">Pilih Kelas</option>
+			<?php 
+			include "conn.php";
+			
+			$query=mysql_query("select * from kelas order by nama_kelas asc",$koneksi);
+			
+			while($row=mysql_fetch_array($query))
+			{
+				?><option value="<?php  echo $row['kd_kelas']; ?>"><?php  echo $row['nama_kelas']; ?></option><?php 
+			}
+			?>
+			</select>	
+			</td>
+		</tr>
+		<tr>
+		<td>Matakuliah</td>
+			<td>
+			<select name="matakuliah">
+			<option value="0" selected="selected">Pilih MK</option>
+			<?php 
+			include "conn.php";
+			
+			$query=mysql_query("select * from matakuliah order by nm_mk asc",$koneksi);
+			
+			while($row=mysql_fetch_array($query))
+			{
+				?><option value="<?php  echo $row['kd_mk']; ?>"><?php  echo $row['nm_mk']; ?></option><?php 
+			}
+			?>
+			</select>	
+			</td>
+			<tr>
+			<td>VerIifikasi Dosen
+			<?php
+				echo "<input type=checkbox name=hadir[] value=$row[kd_dosen] id='$no'>";
+				$no++;
+				?>
+			</td>
+			
+			<td>Verifikasi PK 
+
+			<?php
+				echo "<input type=checkbox name=hadir[] value=$row[pk] id=$no>";
+				$no++;
+				?>
+			</td>
+			
+			
+			
+		
+		<tr>
 			<th>No</th>
+			<th>Tanggal</th>
 			<th>Materi</th>
 			<th>Verifikasi Dosen</th>
 			<th>Verfikasi PK</th>
-			<th>Jumlah Mahasiswa</th>
 		</tr>
 		<?php
 		
@@ -30,43 +88,23 @@
 		?>
 		<tr>
 			<td><?php echo $c=$c+1;?></td>
+			<td><?php echo $row['tgl_jurnal'];?></td>
 			<td><?php echo $row['materi'];?></td>
-			<td align="center">
-				<?php
-				echo "<input type=checkbox name=hadir[] value=$row[kd_dosen] id='$no'>";
-				$no++;
-				?>
-			</td>
-			<td align="center">
-				<?php
-				echo "<input type=checkbox name=hadir[] value=$row[pk] id=$no>";
-				$no++;
-				?>
-			</td>
-			<td></td>
+				<td><?php echo $row['ver_dosen'];?></td>
+			<td><?php echo $row['pk'];?></td>
 			
 		</tr>
 		<?php
 		}
 		
-		echo "
-			<tr>
-				<td></td>
-				<td></td>
-				<td align=center>
-				<input type='button' name='pilih' onclick='for (i=0;i<$no;i++){document.getElementById(i).checked=true;}' value='Check All'>
-				</td>
-				<td align=center>
-				<input type='button' name='pilih' onclick='for (i=0;i<$no;i++){document.getElementById(i).checked=false;}' value='Uncheck All'>
-				</td>
-				<td></td>
-			</tr>";
+		
 		?>
 		</table>
-		<br />
-		<input type="checkbox" name="selesai" value="yes" />Tandai Kelas Selesai
-		<br /><br />
-		<input type="submit" value="Input" />
+		<tr></tr>
+		<tr>
+			<input type="submit" value="Simpan" />
+		</tr>
+	
 		</form>
 		</p>
   </div>
