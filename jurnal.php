238bfa9668ce7f0dc3faa0cdbf953f3bf826608f
gilang samodra <gilangsamoedra08@gmail.com>
2015-06-17 12:02:12 +0700

    desain login dan jurnal

--- jurnal.php
+++ jurnal.php
@@ -23,13 +23,16 @@
 		
 		while($row=mysql_fetch_array($matakuliah)){
 			//mencari jumlah siswa masing2 mk
+			$kd_dosen=$_GET['kd_dosen'];
 			$siswa=mysql_query("select * from siswa where kd_mk='$row[kd_mk]'",$koneksi);
 			$jumlah=mysql_num_rows($siswa);
 			?>
+			
 			<tr>
+			
 				<td align="center"><?php echo $c=$c+1; ?></td>
 				<td align="center"><?php echo $row['nm_mk']; ?></td>
-				<td align="center"><?php echo $row['dosen']; ?></td>
+				<td align="center"><?php echo $row['kd_dosen']; ?></td>
 				<td align="center"><?php echo $jumlah;?> Orang</td>
 				<td align="center"><a href="?page=input_absensi&kd_mk=<?php echo $row['kd_mk'];?>">Absensi</a>
 				<a href="?page=input_jurnal&kd_mk=<?php echo $row['kd_mk'];?>">Jurnal</a>
